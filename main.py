2a9ba2e861c8d5d75e2e436ce40234c54951305d
ragul66 <ragul1.gokul2.3@gmail.com>
2026-07-14 22:31:01 +0530

    commit for the cross origins

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app = FastAPI(title="Real-Time Developer Skill-Swap Platform API")
 # Configure CORS (Support local frontend port 5173 and 5174)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5175", "http://127.0.0.1:5175"],
+    allow_origins=[FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174", "http://localhost:5175", "http://127.0.0.1:5175", "https://skillswap-sigma-self.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
